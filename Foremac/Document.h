03b24f17177cc45483c30810925b696cc85f0bc7
Michael Nutt <michael@nuttnet.net>
2014-08-02 15:09:53 -0400

    Initial Commit

---
+++ Foremac/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Foremac
+//
+//  Created by Michael Nutt on 8/2/14.
+//  Copyright (c) 2014 nutt.im. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Foremac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Foremac
+//
+//  Created by Michael Nutt on 8/2/14.
+//  Copyright (c) 2014 nutt.im. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
